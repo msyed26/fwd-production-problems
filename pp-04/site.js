39b6ba35f176ba9a35c6a0c9a33cd66e431a1845
msyed26 <msyed26@hawk.iit.edu>
2021-11-19 04:07:49 -0600

    Complete step 3

--- pp-04/site.js
+++ pp-04/site.js
@@ -25,15 +25,30 @@ console.log('The value of x is', x, '-- it should be 5.');
   can accurately test your work.
 */
 
-var double = function(num) {
-  var x = num * 2;
-  return x;
-};
-double(6);
-console.log('The value of x is', x, '-- it should be 5.');
+// var double = function(num) {
+//   var x = num * 2;
+//   return x;
+// };
+// double(6);
+// console.log('The value of x is', x, '-- it should be 5.');
 
 /*
   Step 3. Rewrite your corrected `double()` function from Step 2.
   so that non-number values passed into the function are handled
   in some reasonable way.
 */
+var double = function(val) {
+  if (typeof(val) == 'number') {
+  var num = val * 2;
+  return num;
+} else {
+  return val + val;
+}
+};
+
+var num = double(6);
+console.log("The value of the input is", num);
+var words = double("Lorem ipsum Dr.Stolley lol");
+console.log("The value of the input is", words);
+console.log("No number detected")
+console.log("The value of x is', x, '-- it should be 5.");
